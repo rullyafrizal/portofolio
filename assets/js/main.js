8d4ce95cddb9b59e47095c2bf39fc2d72695e7f9
rullyafrizal <rullyafrizal4@gmail.com>
2022-01-21 12:40:43 +0700

    update 21 jan 2022

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,62 +1,62 @@
-/*===== MENU SHOW =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+// /*===== MENU SHOW =====*/ 
+// const showMenu = (toggleId, navId) =>{
+//     const toggle = document.getElementById(toggleId),
+//     nav = document.getElementById(navId)
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
+//     if(toggle && nav){
+//         toggle.addEventListener('click', ()=>{
+//             nav.classList.toggle('show')
+//         })
+//     }
+// }
+// showMenu('nav-toggle','nav-menu')
 
-/*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+// /*===== ACTIVE AND REMOVE MENU =====*/
+// const navLink = document.querySelectorAll('.nav__link');   
 
-function linkAction(){
-  /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
+// function linkAction(){
+//   /*Active link*/
+//   navLink.forEach(n => n.classList.remove('active'));
+//   this.classList.add('active');
   
-  /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction));
+//   /*Remove menu mobile*/
+//   const navMenu = document.getElementById('nav-menu')
+//   navMenu.classList.remove('show')
+// }
+// navLink.forEach(n => n.addEventListener('click', linkAction));
 
-/*===== SCROLL REVEAL ANIMATION =====*/
-const sr = ScrollReveal({
-    origin: 'top',
-    distance: '150px',
-    duration: 1000,
-    reset: true
-});
+// /*===== SCROLL REVEAL ANIMATION =====*/
+// const sr = ScrollReveal({
+//     origin: 'top',
+//     distance: '150px',
+//     duration: 1000,
+//     reset: true
+// });
 
-/*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 30}); 
-sr.reveal('.home__img',{delay: 30}); 
-sr.reveal('.home__social-icon',{ interval: 30}); 
+// /*SCROLL HOME*/
+// sr.reveal('.home__title',{}); 
+// sr.reveal('.button',{delay: 30}); 
+// sr.reveal('.home__img',{delay: 30}); 
+// sr.reveal('.home__social-icon',{ interval: 30}); 
 
-/*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 30}); 
-sr.reveal('.about__text',{delay: 30}); 
+// /*SCROLL ABOUT*/
+// sr.reveal('.about__img',{}); 
+// sr.reveal('.about__subtitle',{delay: 30}); 
+// sr.reveal('.about__text',{delay: 30}); 
 
-/*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 30}); 
-sr.reveal('.skills__img',{delay: 30});
+// /*SCROLL SKILLS*/
+// sr.reveal('.skills__subtitle',{}); 
+// sr.reveal('.skills__text',{}); 
+// sr.reveal('.skills__data',{interval: 30}); 
+// sr.reveal('.skills__img',{delay: 30});
 
-/*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 30});
-sr.reveal('.work__img2',{interval: 30}); 
+// /*SCROLL WORK*/
+// sr.reveal('.work__img',{interval: 30});
+// sr.reveal('.work__img2',{interval: 30}); 
 
-const now = new Date(Date.now());
-const year = now.getFullYear();
-const footerCr = document.getElementById('footer__cr').innerHTML = `&#169; ${year} - With love &hearts;`;
+// const now = new Date(Date.now());
+// const year = now.getFullYear();
+// const footerCr = document.getElementById('footer__cr').innerHTML = `&#169; ${year} - With love &hearts;`;
 
 
 
